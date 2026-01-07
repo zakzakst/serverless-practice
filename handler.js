1417780c09f5d18196580d43c78fa0299ca5e652
zakzakst <e1433104@gmail.com>
2026-01-08 08:18:01 +0900

    fix

--- handler.js
+++ handler.js
@@ -1,16 +1,21 @@
 'use strict';
 
 module.exports.hello = async (event) => {
+  const name = event.queryStringParameters?.name ?? 'World';
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    // body: JSON.stringify(
+    //   {
+    //     message: 'Go Serverless v1.0! Your function executed successfully!',
+    //     input: event,
+    //   },
+    //   null,
+    //   2
+    // ),
+    body: JSON.stringify({
+      message: `Hello ${name}`,
+    }),
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
