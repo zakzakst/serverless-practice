271e293b0121a7e8cfe3ab07ef46c71c385691a1
zakzakst <e1433104@gmail.com>
2026-01-18 14:44:57 +0900

    fix

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # serverless-practice
+
+## メモ
+- ローカルで実行するコマンド
+    - `npx serverless invoke local -f costNotifier`
---
+++ src/myHello.js
@@ -0,0 +1,5 @@
+export async function handler(event, context) {
+    return {
+        message: 'Hello Serverless Framework'
+    }
+}
